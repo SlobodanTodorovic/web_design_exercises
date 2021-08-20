fc096ce79a03566410dd4bed412df00b10c52c5e
SlobodanTodorovic <bobakpd88@yahoo.com>
2021-08-21 00:16:30 +0200

    slider-fixed withnot fake images

--- bubanjski/bubanjski.js
+++ bubanjski/bubanjski.js
@@ -1,62 +1,42 @@
-let sliderImages = ['file:///home/bobakpd/Documents/web_desing/bubanjski/slika1.jpg','file:///home/bobakpd/Documents/web_desing/bubanjski/slika2.jpg','file:///home/bobakpd/Documents/web_desing/bubanjski/slika3.jpg','file:///home/bobakpd/Documents/web_desing/bubanjski/slika4.jpg','file:///home/bobakpd/Documents/web_desing/bubanjski/slika5.jpg']
-let sliderArrowRight= document.querySelector('#slider-arrow-right')
-let sliderArrowLeft= document.querySelector('#slider-arrow-left')
-let sliderImage=document.querySelector('#slider-img')
-let sliderImageIndex = 0
-const sliderContainer= document.querySelector('.slider-container')
-const sliderNavs=document.querySelector('.slider-navs')
-sliderContainer.addEventListener('mouseover',function (e) {
-    sliderNavs.style.opacity='1'
+const slider=document.querySelector('.slider')
+const sliderContainer=document.querySelector('.slider-container')
+let sliderPosition=0
+const sliderRight=document.querySelector('#slider-arrow-right')
+const sliderLeft=document.querySelector('#slider-arrow-left')
+function sliderGo() {
+    if (sliderPosition===-400) {
+        sliderPosition=0
+    }else{
+        sliderPosition-=100
+    }   
+    slider.style.translate=`${sliderPosition}%`
+}
+let id = setInterval(sliderGo,2000)
+sliderContainer.addEventListener('mouseenter',function () {    
     clearInterval(id)
 })
-sliderContainer.addEventListener('mouseout',function (e) {
-    sliderNavs.style.opacity='0'
-    id=setInterval(slideToRight,3000)
+sliderContainer.addEventListener('mouseleave',function(){    
+    id=setInterval(sliderGo,2000)
 })
-const slideToRight = function () {  
-    if (sliderImageIndex===sliderImages.length-1) {
-        sliderImageIndex=0
+sliderRight.addEventListener('click',function (evt) {    
+    if (sliderPosition===-400) {
+        sliderPosition=0
     }else{
-        sliderImageIndex+=1
-    }      
-    sliderImage.setAttribute('src',`${sliderImages[sliderImageIndex]}`)
-    sliderDots[sliderImageIndex].classList.toggle('slider-dot-colored')
-    if (sliderImageIndex===0) {
-        sliderDots[sliderDots.length-1].classList.toggle('slider-dot-colored')
-    }else{
-        sliderDots[sliderImageIndex-1].classList.toggle('slider-dot-colored')
-    }
+        sliderPosition-=100
+    }   
+    slider.style.translate=`${sliderPosition}%`
     
-}   
+})
+sliderLeft.addEventListener('click',function (evt) {    
+    if (sliderPosition===0) {
+        sliderPosition=-400
+    }else{
+        sliderPosition+=100
+    }    
+    slider.style.translate=`${sliderPosition}%`
+    
+})
 
-const slideToLeft = function () {
-    if (sliderImageIndex===0) {
-        sliderImageIndex=sliderImages.length-1
-    }else {
-        sliderImageIndex-=1
-    }
-    sliderImage.setAttribute('src',`${sliderImages[sliderImageIndex]}`)
-    sliderDots[sliderImageIndex].classList.toggle('slider-dot-colored')
-    if (sliderImageIndex===sliderDots.length-1) {
-        sliderDots[0].classList.toggle('slider-dot-colored')
-    }else {
-        sliderDots[sliderImageIndex+1].classList.toggle('slider-dot-colored')
-    }
-}
-let id=setInterval(slideToRight,2000)
-
-
-
-sliderArrowRight.addEventListener('click',slideToRight )
-sliderArrowLeft.addEventListener('click',slideToLeft )
-const dotsContainer= document.querySelector('.slider-dots')
-for (let i = 0; i < sliderImages.length; i++) {
-    const newDot =document.createElement('span')
-    newDot.classList.add('slider-dot')
-    dotsContainer.appendChild(newDot)    
-}
-const sliderDots= document.querySelectorAll('.slider-dot')
-sliderDots[0].classList.add('slider-dot-colored')
 
 const navLis= document.querySelectorAll('.nav >li')
 for (const li of navLis) {      
